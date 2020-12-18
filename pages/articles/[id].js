48858c5de65a710846bea511f4595fe8891e4ff8
Jerry Mesimäki <jerry.mesimaki@yle.fi>
2020-12-18 21:15:36 +0200

    Add some conditions to fix the build

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -1,10 +1,11 @@
 import axios from "axios";
-import Link from 'next/link'
+import Link from "next/link";
+import get from "lodash/get";
 import ArticleSerializer from "../../components/article-serializer";
 import config from "../../config";
 
 export default function Article({ article }) {
-  const title = article.fields.title;
+  const title = get(article, 'fields.title', '')
   return (
     <>
       <h1>
@@ -26,6 +27,7 @@ export async function getStaticProps({ params }) {
   const article = data.items.find(
     (contentfulItem) => contentfulItem.sys.id === params.id
   );
+
   return {
     props: {
       article,
@@ -34,18 +36,15 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  console.log(config)
-  
   const { data } = await axios.get(
     `https://cdn.contentful.com/spaces/${config.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${config.CONTENTFUL_DELIVERY_TOKEN}`
   );
-
-  console.log(JSON.stringify(data))
 
   const frontPage = data.items.find(
     ({ sys: contentfulItem }) =>
       contentfulItem.contentType.sys.id === "frontPage"
   );
+
   const topHeadlineIds = frontPage.fields.topList.map(
     (topListArticle) => topListArticle.sys.id
   );
